531240653f70b8b95756eaa9cb01311cf5c100ff
Your Name <you@example.com>
2025-06-05 15:44:15 +0530

    update project

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://e-commerce-backend-qtfu.onrender.com",
+  baseURL: "http://localhost:3001/api/",
 });
 
 export const UserSignUp = async (data) => await API.post("/user/signup", data);
